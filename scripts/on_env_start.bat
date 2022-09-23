6bc906ce3ce9bcedae00bd2c47f17c0feaca4af7
alanine <free-tip@hotmail.com>
2022-09-23 15:16:11 -0300

    Update on_env_start.bat

--- scripts/on_env_start.bat
+++ scripts/on_env_start.bat
@@ -1,37 +1,50 @@
 @echo off
 
-@echo. & echo "Stable Diffusion UI - v2.1" & echo.
+@echo. & echo "astica Art Creator v0.9" & echo.
 
 @cd ..
 
-for /f "tokens=*" %%a in ('python -c "import os; parts = os.getcwd().split(os.path.sep); print(len(parts))"') do if "%%a" NEQ "2" (
-    echo. & echo "!!!! WARNING !!!!" & echo.
-    echo "Your 'stable-diffusion-ui' folder is at %cd%" & echo.
-    echo "The 'stable-diffusion-ui' folder needs to be at the top of your drive, for e.g. 'C:\stable-diffusion-ui' or 'D:\stable-diffusion-ui' etc."
-    echo "Not placing this folder at the top of a drive can cause errors on some computers."
-    echo. & echo "Recommended: Please close this window and move the 'stable-diffusion-ui' folder to the top of a drive. For e.g. 'C:\stable-diffusion-ui'. Then run the installer again." & echo.
-    echo "Not Recommended: If you're sure that you want to install at the current location, please press any key to continue." & echo.
+if exist "scripts\config.bat" (
+    @call scripts\config.bat
+)
 
-    pause
+if "%update_branch%"=="" (
+    set update_branch=main
+)
+
+@>nul grep -c "conda_sd_ui_deps_installed" scripts\install_status.txt
+@if "%ERRORLEVEL%" NEQ "0" (
+    for /f "tokens=*" %%a in ('python -c "import os; parts = os.getcwd().split(os.path.sep); print(len(parts))"') do if "%%a" NEQ "2" (
+        echo. & echo "!!!! WARNING !!!!" & echo.
+        echo "Your 'astica-art-creator' folder is at %cd%" & echo.
+        echo "The 'astica-art-creator' folder needs to be at the top of your drive, for e.g. 'C:\astica-art-creator' or 'D:\astica-art-creator' etc."
+        echo "Not placing this folder at the top of a drive can cause errors on some computers."
+        echo. & echo "Recommended: Please close this window and move the 'astica-art-creator' folder to the top of a drive. For e.g. 'C:\astica-art-creator'. Then run the installer again." & echo.
+        echo "Not Recommended: If you're sure that you want to install at the current location, please press any key to continue." & echo.
+
+        pause
+    )
 )
 
 @>nul grep -c "sd_ui_git_cloned" scripts\install_status.txt
 @if "%ERRORLEVEL%" EQU "0" (
-    @echo "Stable Diffusion UI's git repository was already installed. Updating.."
+    @echo "astica Art Creator's git repository was already installed. Updating from %update_branch%.."
 
     @cd sd-ui-files
 
     @call git reset --hard
+    @call git checkout "%update_branch%"
     @call git pull
 
     @cd ..
 ) else (
-    @echo. & echo "Downloading Stable Diffusion UI.." & echo.
+    @echo. & echo "Downloading astica Art Creator.." & echo.
+    @echo "Using the %update_branch% channel" & echo.
 
-    @call git clone https://github.com/cmdr2/stable-diffusion-ui.git sd-ui-files && (
+    @call git clone -b "%update_branch%" https://github.com/alanine/art-creator-installer.git sd-ui-files && (
         @echo sd_ui_git_cloned >> scripts\install_status.txt
     ) || (
-        @echo "Error downloading Stable Diffusion UI. Please try re-running this installer. If it doesn't work, please copy the messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB or file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues"
+        @echo "Error downloading astica Art Creator. Sorry about that, please try to:" & echo "  1. Run this installer again." & echo "Thanks!"
         pause
         @exit /b
     )
@@ -39,6 +52,8 @@ for /f "tokens=*" %%a in ('python -c "import os; parts = os.getcwd().split(os.pa
 
 @xcopy sd-ui-files\ui ui /s /i /Y
 @copy sd-ui-files\scripts\on_sd_start.bat scripts\ /Y
-@copy "sd-ui-files\scripts\Start Stable Diffusion UI.cmd" . /Y
+@copy "sd-ui-files\scripts\Start astica_Art_Creator.cmd" . /Y
 
 @call scripts\on_sd_start.bat
+
+@pause
